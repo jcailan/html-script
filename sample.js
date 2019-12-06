e39b2af2b3f8e9db72a1701462d8177def701e2c
Jhodel Cailan <jhodel.cailan@gmail.com>
2019-12-06 16:24:14 +0800

    Create sample.js

---
+++ sample.js
@@ -0,0 +1,12 @@
+function getElementsByIdAndClassName(sId, sClassName) {
+    var aResult = []; debugger;
+    var aElements = document.getElementsByClassName(sClassName);
+    for (var i=0; i < aElements.length; i++) {
+        if (aElements[i].id === sId) {
+            aResult.push(aElements[i]);
+        }
+    }
+    return aResult;
+}
+
+getElementsByIdAndClassName("edit-bg", "ep-enhanced")
